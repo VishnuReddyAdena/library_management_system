8e6dbd436ba4582d7f45b3d1e78ce1a6514f2c56
VishnuReddyAdena <vishnureddyadena7@gmail.com>
2026-05-30 07:37:59 +0530

    Clean: remove obsolete Python/Flask files from backend models and routes

--- backend/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from extensions import db
-
-# Export all models for easier importing
-from .user import User, AuditLog
-from .library import Publisher, Author, Category, Book, Member, Transaction, PurchaseOrder
